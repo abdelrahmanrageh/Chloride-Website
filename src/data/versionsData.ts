8d32badcf3e7e9776180aac9ac31f1a982ebe49f
abdorageh <abdelrahmanrageh@gmail.com>
2024-10-06 22:17:52 +0300

    Implementing new versions add/remove

--- src/data/versionsData.ts
+++ src/data/versionsData.ts
@@ -3,34 +3,36 @@ export const versionsNames = ["windows10", "windows11", "tweaks"];
 export const versions = {
   windows10Versions: [
     {
-      version: "Beta 1.0.0 22h2 ",
+      version: "ChlorideOS 21H2 Enterprise LTSC Extreme",
+      suitableFor: "Best Option for you",
+      whatsNew:
+        "0 bloatwares ,lower cpu processes ( 50 - 60 ) , lower ram usage ( 0.7 - 0.9 GB ), 3.4GB iso",
+      price: "Free",
+      link: "https://mega.nz/folder/5800zSpJ#wP1fJCt9yC_xpdWZfo0UAw",
+    },
+    {
+      version: "ChlorideOS Beta 1.0.0 22h2",
       suitableFor: "Simple Daily Use",
       whatsNew:
         "0 bloatwares ,lower cpu processes ( 35 - 50 ) , lower ram usage ( 1.3 GB ), No Bluetooth , No Printing , 3.9GB iso",
       price: "Free",
       link: "https://www.mediafire.com/folder/de2p58pxbw1fr/Free+ChlorideO+10+1.0.0+22h2",
     },
+
     {
-      version: "ChlorideOS 10 Enterprise LTSC Extreme ",
-      suitableFor: "Best Option for you",
+      version: "ChlorideOS 1809",
+      suitableFor: "Best For Low End Devices",
       whatsNew:
-        "0 bloatwares ,lower cpu processes ( 50 - 60 ) , lower ram usage ( 0.7 - 0.9 GB ), 3.6GB iso",
+        "80% Of bloatwares removed , lower cpu process ( 45 - 60 ) , lower ram usage ( 1.0 - 1.3 GB ), Only 7-8 GB of disk usage, Chloride Tweaks, 2.6GB iso ",
       price: "Free",
-      link: "https://www.youtube.com/@ChlorideOS/videos",
+      link: "https://www.mediafire.com/file/wfwsb5xfe6v1nmw/ChlorideOS_10_1809_SUPER_LIGHT.rar/file",
     },
+
     {
-      version: "1.0.0 21H2 LTSC",
-      suitableFor: "Best For Gaming",
+      version: "ChlorideOS 1909 x64 Extreme",
+      suitableFor: "Lightest Windows10 Ever",
       whatsNew:
-        "0 bloatwares , lower cpu process ( 55 - 65 ) , lower ram usage ( 1.0 - 1.3 GB ), Only 7-8 GB of disk usage, Chloride Tweaks , Chloride Post Install Folder, 3.6GB iso",
-      price: "With Tweaks Ultimate Plan",
-      link: "",
-    },
-    {
-      version: "1.0.0 1809 ",
-      suitableFor: "Lightest Version, Best For Low End Devices",
-      whatsNew:
-        "75% Of bloatwares removed , lower cpu process ( 45 - 60 ) , lower ram usage ( 1.0 - 1.3 GB ), Only 7-8 GB of disk usage, Chloride Tweaks , Chloride Post Install Folder, 2.6GB iso With Tweaks Ultimate Plan",
+        "0 bloatwares, lower cpu process ( 40-55 ) , lower ram usage ( 0.5 - 0.7 GB ), Only 4-5 GB of disk usage, Chloride Post Install Folder, 1.2GB iso",
       price: "With Tweaks Ultimate Plan",
       link: "https://chlorideos.netlify.app/",
     },
@@ -38,12 +40,20 @@ export const versions = {
 
   windows11Versions: [
     {
-      version: "Beta 1.0.0 22h2",
+      version: "ChlorideOS 22h2 Debloated",
       suitableFor: "Simple Daily Use",
       whatsNew:
-        "0 bloatwares ,lower cpu processes ( 35 - 50 ) , lower ram usage ( 1.3 GB ), No Bluetooth , No Printing , 3.9GB iso",
+        "80% Of bloatwares removed ,lower cpu processes ( 80 - 100 ) , lower ram usage ( 1.3 GB ), 2.6GB iso",
       price: "Free",
       link: "https://mega.nz/folder/opVQkLoB#I2Ths0zhZ9wdPBcihrvElA",
+    },
+    {
+      version: "ChlorideOS 23h2 Debloated",
+      suitableFor: "Simple Daily Use",
+      whatsNew:
+        "70% Of bloatwares removed ,low after tweaking cpu processes ( 60 - 70 ) , lowe ram usage ( 1.7 GB ), 4.6GB iso",
+      price: "Free",
+      link: "https://mega.nz/file/wgUF1KIQ#9vbMZgxmmaO2DIVZWJIA98QxrEsJEQV5MG6A70Knn8Y",
     },
     {
       version: "1.0.0 LTSC",
@@ -73,19 +83,10 @@ export const versions = {
       link: "https://www.mediafire.com/folder/t32zpuvdwzrjd/Tweaks",
     },
     {
-      version: "Gold Plan",
-      suitableFor: "",
-      whatsNew: `Unlock up to 35% FPS boost with 450 powerful tools and 615MB of scripts.
-          Get custom Windows tweaks for performance, privacy, power, BIOS, and more.
-          Remove bloatware, optimize the registry, and enhance efficiency with advanced power optimization.`,
-      price: "3$",
-      link: "https://chlorideos.netlify.app/",
-    },
-    {
       version: "Ultimate Plan",
       suitableFor: "",
       whatsNew: `
-        Boost FPS by 35% with 450 premium tools and 615MB of advanced scripts. Get 100% free access to all ChlorideOS versions,
+         Get 100% free access to all ChlorideOS versions, Boost FPS by 35% with 450 premium tools and 615MB of advanced scripts,
         offering optimizations for performance, privacy, network, power, BIOS, GPU, USB, and more. Remove unnecessary apps,
         optimize the registry, and enjoy deep PC debloating for a streamlined, efficient system. Elevate performance with
         advanced power optimization techniques.`,
@@ -93,7 +94,4 @@ export const versions = {
       link: "https://chlorideos.netlify.app/",
     },
   ],
-
- 
-
 };
