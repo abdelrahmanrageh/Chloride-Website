f8e07fea07df66975b508c217fab5f053c2bb60e
ChlorideOS <86505707+ArabWep@users.noreply.github.com>
2024-10-13 05:30:37 -0700

    Update versionsData.ts

--- src/data/versionsData.ts
+++ src/data/versionsData.ts
@@ -1,159 +1,133 @@
 export const versionsNames = ["AtomOS", "ChlorideOS10", "ChlorideOS11", "tweaks", "OtherCustomReleases", "ChlorideArchive"];
 
 export const versions = {
+    AtomOS: [
+        {
+            version: "AtomOS 11 23h2 Standard",
+            suitableFor: "Lightest Windows 11 release",
+            whatsNew: "0 bloatwares, lower CPU processes after tweaking (50 - 60), lower RAM usage (0.9 - 1.2 GB), 1.8GB ISO",
+            price: "Free",
+            link: "https://mega.nz/file/Z4k2VQ5S#-B7wY1-kG4M6EDErUqzZBtzZma53e0D4RXbf5s7DM1E"
+        }
+    ],
 
-    AtomOSVersions: [
-    {
-      version: "AtomOS 11 23h2 Standard ",
-      suitableFor: "Lighest windows 11 release",
-      whatsNew:
-        "0 bloatwares ,lower cpu processes after tweaking ( 50 - 60 ) , lower ram usage ( 0.9 - 1.2 GB ), 1.8GB iso",
-      price: "Free",
-      link: "https://mega.nz/file/Z4k2VQ5S#-B7wY1-kG4M6EDErUqzZBtzZma53e0D4RXbf5s7DM1E",
-    },
-  ],
+    ChlorideOS10: [
+        {
+            version: "ChlorideOS 21H2 Enterprise LTSC Extreme",
+            suitableFor: "Best Option for you",
+            whatsNew: "0 bloatwares, lower CPU processes after tweaking (50 - 60), lower RAM usage (0.7 - 0.9 GB), 3.4GB ISO",
+            price: "Free",
+            link: "https://mega.nz/folder/5800zSpJ#wP1fJCt9yC_xpdWZfo0UAw"
+        },
+        {
+            version: "ChlorideOS 22h2 Debloated Beta",
+            suitableFor: "Simple Daily Use",
+            whatsNew: "0 bloatwares, lower CPU processes after tweaking (35 - 50), lower RAM usage (1.3 GB), No Bluetooth, No Printing, 3.9GB ISO",
+            price: "Free",
+            link: "https://www.mediafire.com/folder/de2p58pxbw1fr/Free+ChlorideO+10+1.0.0+22h2"
+        },
+        {
+            version: "ChlorideOS 1809 Debloated",
+            suitableFor: "Best For Low-End Devices",
+            whatsNew: "80% Of bloatwares removed, lower CPU process after tweaking (45 - 60), lower RAM usage (1.0 - 1.3 GB), Only 7-8 GB of disk usage, Chloride Tweaks, 2.6GB ISO",
+            price: "Free",
+            link: "https://www.mediafire.com/folder/m9tn5maskec0g/ChlorideOS+10+1809+SUPER+LIGHT"
+        },
+        {
+            version: "ChlorideOS 1909 Ultimate",
+            suitableFor: "Lightest Windows 10 Ever",
+            whatsNew: "0 bloatwares, lower CPU process after tweaking (40-55), lower RAM usage (0.5 - 0.7 GB), Only 4-5 GB of disk usage, Chloride Post Install Folder, 1.2GB ISO",
+            price: "With Tweaks Ultimate Plan",
+            link: "https://chlorideos.netlify.app/"
+        },
+        {
+            version: "ChlorideOS 21h2 LTSC Ultimate",
+            suitableFor: "Best & most stable Windows 10 you can find",
+            whatsNew: "0 bloatwares, lower CPU process after tweaking (40-50), lower RAM usage (0.5 - 0.8 GB), Only 7-8 GB of disk usage, 3.4GB ISO",
+            price: "With Tweaks Ultimate Plan",
+            link: "https://chlorideos.netlify.app/"
+        }
+    ],
 
+    ChlorideOS11: [
+        {
+            version: "ChlorideOS 22h2 Debloated",
+            suitableFor: "Simple Daily Use",
+            whatsNew: "80% Of bloatwares removed, lower CPU processes (80 - 100), lower RAM usage (1.4 GB), 2.9GB ISO",
+            price: "Free",
+            link: "https://mega.nz/folder/opVQkLoB#I2Ths0zhZ9wdPBcihrvElA"
+        },
+        {
+            version: "ChlorideOS 23h2 Debloated",
+            suitableFor: "Simple Daily Use",
+            whatsNew: "70% Of bloatwares removed, lower CPU processes after tweaking (60 - 70), lower RAM usage (1.7 GB), 4.6GB ISO",
+            price: "Free",
+            link: "https://mega.nz/file/wgUF1KIQ#9vbMZgxmmaO2DIVZWJIA98QxrEsJEQV5MG6A70Knn8Y"
+        },
+        {
+            version: "ChlorideOS 22H2 Extreme",
+            suitableFor: "Lightest Version Ever, Best For Low-End Devices",
+            whatsNew: "100% Of bloatwares removed, lower CPU processes (70-80), lower RAM usage (1.0 - 1.3 GB), Only 9-10 GB of disk usage, Chloride Tweaks, Chloride Post Install Folder, 2.8GB ISO",
+            price: "With Tweaks Ultimate Plan",
+            link: "https://chlorideos.netlify.app/"
+        }
+    ],
 
+    tweaks: [
+        {
+            version: "Standard Plan",
+            suitableFor: "General Performance Boost",
+            whatsNew: "Gain up to 20% FPS boost with 25 essential tools and 100MB of scripts. Enjoy performance, network, power tweaks, GPU optimization, registry enhancements, deep PC debloating, and tailored Fortnite optimization for a competitive edge.",
+            price: "Free",
+            link: "https://www.mediafire.com/folder/t32zpuvdwzrjd/Tweaks"
+        },
+        {
+            version: "Ultimate Plan",
+            suitableFor: "Advanced Performance and Customization",
+            whatsNew: "Get 100% free access to all ChlorideOS versions. Boost FPS by 35% with 450 premium tools and 615MB of advanced scripts, offering optimizations for performance, privacy, network, power, BIOS, GPU, USB, and more. Remove unnecessary apps, optimize the registry, and enjoy deep PC debloating for a streamlined, efficient system. Elevate performance with advanced power optimization techniques.",
+            price: "$5",
+            link: "https://chlorideos.netlify.app/"
+        }
+    ],
 
+    OtherCustomReleases: [
+        {
+            version: "AtlasOS11 23h2 ISO",
+            suitableFor: "Custom Build",
+            whatsNew: "AtlasOS 11 23H2 (ISO download) is here! This new release has been rebuilt using VMware for project, AME Wizard Beta, and AtlasOS playbook for creating AtlasOS.",
+            price: "Free",
+            link: "https://archive.org/details/atlasos1123h2/Screenshot%20(2).png"
+        },
+        {
+            version: "AtlasOS10 20h2 ISO",
+            suitableFor: "Custom Build",
+            whatsNew: "AtlasOS 10 20H2 (ISO download) is here! This new release has been rebuilt using VMware for project, AME Wizard Beta, and AtlasOS playbook for creating AtlasOS.",
+            price: "Free",
+            link: "https://archive.org/details/atlas-v0.4.2/1_desktop.png"
+        }
+    ],
 
-
-
-  
-  ChlorideOS10Versions: [
-    {
-      version: "ChlorideOS 21H2 Enterprise LTSC Extreme ",
-      suitableFor: "Best Option for you",
-      whatsNew:
-        "0 bloatwares ,lower cpu processes after tweaking ( 50 - 60 ) , lower ram usage ( 0.7 - 0.9 GB ), 3.4GB iso",
-      price: "Free",
-      link: "https://mega.nz/folder/5800zSpJ#wP1fJCt9yC_xpdWZfo0UAw",
-    },
-    {
-      version: "ChlorideOS 22h2 Debloated Beta",
-      suitableFor: "Simple Daily Use",
-      whatsNew:
-        "0 bloatwares ,lower cpu processes after tweaking ( 35 - 50 ) , lower ram usage ( 1.3 GB ), No Bluetooth , No Printing , 3.9GB iso",
-      price: "Free",
-      link: "https://www.mediafire.com/folder/de2p58pxbw1fr/Free+ChlorideO+10+1.0.0+22h2",
-    },
-
-    {
-      version: "ChlorideOS 1809 Debloated",
-      suitableFor: "Best For Low End Devices",
-      whatsNew:
-        "80% Of bloatwares removed , lower cpu process after tweaking ( 45 - 60 ) , lower ram usage ( 1.0 - 1.3 GB ), Only 7-8 GB of disk usage, Chloride Tweaks, 2.6GB iso ",
-      price: "Free",
-      link: "https://www.mediafire.com/folder/m9tn5maskec0g/ChlorideOS+10+1809+SUPER+LIGHT",
-    },
-
-    {
-      version: "ChlorideOS 1909 Ultimate",
-      suitableFor: "Lightest Windows10 Ever",
-      whatsNew:
-        "0 bloatwares, lower cpu process after tweaking ( 40-55 ) , lower ram usage ( 0.5 - 0.7 GB ), Only 4-5 GB of disk usage, Chloride Post Install Folder, 1.2GB iso",
-      price: "With Tweaks Ultimate Plan",
-      link: "https://chlorideos.netlify.app/",
-    },
-    {
-      version: "ChlorideOS 21h2 LTSC Ultimate",
-      suitableFor: "Best & most stable Windows10 you can find",
-      whatsNew:
-        "0 bloatwares, lower cpu process after tweaking ( 40-50 ) , lower ram usage ( 0.5 - 0.8 GB ), Only 7-8 GB of disk usage, 3.4GB iso",
-      price: "With Tweaks Ultimate Plan",
-      link: "https://chlorideos.netlify.app/",
-    },
-  ],
-
-  ChlorideOS11Versions: [
-    {
-      version: "ChlorideOS 22h2 Debloated",
-      suitableFor: "Simple Daily Use",
-      whatsNew:
-        "80% Of bloatwares removed ,lower cpu processes ( 80 - 100 ) , lower ram usage ( 1.4 GB ), 2.9GB iso",
-      price: "Free",
-      link: "https://mega.nz/folder/opVQkLoB#I2Ths0zhZ9wdPBcihrvElA",
-    },
-    {
-      version: "ChlorideOS 23h2 Debloated",
-      suitableFor: "Simple Daily Use",
-      whatsNew:
-        "70% Of bloatwares removed ,low after tweaking cpu processes ( 60 - 70 ) , lowe ram usage ( 1.7 GB ), 4.6GB iso",
-      price: "Free",
-      link: "https://mega.nz/file/wgUF1KIQ#9vbMZgxmmaO2DIVZWJIA98QxrEsJEQV5MG6A70Knn8Y",
-    },
-    {
-      version: "ChlorideOS 22H2 Extreme ",
-      suitableFor: "Lightest Version Ever, Best For Low End Devices",
-      whatsNew:
-        "100% Of bloatwares removed , lower cpu process ( 70-80 ) , lower ram usage ( 1.0 - 1.3 GB ), Only 9-10 GB of disk usage, Chloride Tweaks , Chloride Post Install Folder, 2.8GB iso",
-      price: "With Tweaks Ultimate Plan",
-      link: "https://chlorideos.netlify.app/",
-    },
-  ],
-
-  tweaksVersions: [
-    {
-      version: "Standard Plan",
-      suitableFor: "",
-      whatsNew: `Gain up to 20% FPS boost with 25 essential tools and 100MB of scripts. Enjoy performance, network, power tweaks,
-       GPU optimization, registry enhancements, deep PC debloating, and tailored Fortnite optimization for a competitive edge.`,
-      price: "Free",
-      link: "https://www.mediafire.com/folder/t32zpuvdwzrjd/Tweaks",
-    },
-    {
-      version: "Ultimate Plan",
-      suitableFor: "",
-      whatsNew: `
-         Get 100% free access to all ChlorideOS versions, Boost FPS by 35% with 450 premium tools and 615MB of advanced scripts,
-        offering optimizations for performance, privacy, network, power, BIOS, GPU, USB, and more. Remove unnecessary apps,
-        optimize the registry, and enjoy deep PC debloating for a streamlined, efficient system. Elevate performance with
-        advanced power optimization techniques.`,
-      price: "5$",
-      link: "https://chlorideos.netlify.app/",
-    },
-  ],
-  
- OtherCustomReleasesVersions: [
-    {
-      version: "AtlasOS11 23h2 iso",
-      suitableFor: "",
-      whatsNew: `AtlasOS 11 23H2 (ISO download) is here!, this new release has been rebuilt using the VMware for project,
-      AME Wizard Beta and AtlasOS playbook for creating AtlasOS! .`,
-      price: "Free",
-      link: "https://archive.org/details/atlasos1123h2/Screenshot%20(2).png",
-    },
-    {
-      version: "AtlasOS10 20h2 iso",
-      suitableFor: "",
-      whatsNew: `AtlasOS 10 20H2 (ISO download) is here!, this new release has been rebuilt using the VMware for project,
-      AME Wizard Beta and AtlasOS playbook for creating AtlasOS! .`,
-      price: "Free",
-      link: "https://archive.org/details/atlas-v0.4.2/1_desktop.png",
-    },
-    
-  ],
-  ChlorideArchiveVersions: [
-    {
-      version: "Chloride processes lower",
-      suitableFor: "",
-      whatsNew: `Get more than 60 cpu process with onlu 1 click! .`,
-      price: "Free",
-      link: "https://www.mediafire.com/file/ytvl1wkmgdnqnxb/Chloride_processes_lower.rar/file",
-    },
-    {
-      version: "ChlorideWallpapers",
-      suitableFor: "",
-      whatsNew: `Get a package of nice ChlorideOS and Microsoft windows 11 style wallpapers! .`,
-      price: "Free",
-      link: "https://www.mediafire.com/folder/iuxuq1i0c4uhg/ChlorideWallpapers",
-    },
-     {
-      version: "Chloride Post Install",
-      suitableFor: "",
-      whatsNew: `A good package contains scripts and tools that can be helpful for you.`,
-      price: "Free",
-      link: "https://www.mediafire.com/file/njzhwk5azy1bmsu/Chloride_Post_install.rar/file",
-    },
-    
-  ],
+    ChlorideArchive: [
+        {
+            version: "Chloride Processes Lower",
+            suitableFor: "Performance Boost",
+            whatsNew: "Get more than 60 CPU processes with only 1 click!",
+            price: "Free",
+            link: "https://www.mediafire.com/file/ytvl1wkmgdnqnxb/Chloride_processes_lower.rar/file"
+        },
+        {
+            version: "Chloride Wallpapers",
+            suitableFor: "Custom Aesthetic",
+            whatsNew: "Get a package of nice ChlorideOS and Microsoft Windows 11 style wallpapers!",
+            price: "Free",
+            link: "https://www.mediafire.com/folder/iuxuq1i0c4uhg/ChlorideWallpapers"
+        },
+        {
+            version: "Chloride Post Install",
+            suitableFor: "Post-Installation Tweaks",
+            whatsNew: "A good package that contains scripts and tools that can be helpful for you.",
+            price: "Free",
+            link: "https://www.mediafire.com/file/njzhwk5azy1bmsu/Chloride_Post_install.rar/file"
+        }
+    ]
 };
