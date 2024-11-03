2f65fe30da0aa0b0b904c0a9c057cdadf505dc20
ChlorideOS <86505707+ArabWep@users.noreply.github.com>
2024-11-03 11:49:13 -0800

    Update VideosSection.tsx

--- src/components/VideosSection.tsx
+++ src/components/VideosSection.tsx
@@ -1,20 +1,18 @@
-// import YouTube from "react-youtube"
-
 import { useState } from "react";
 
 function VideosSection() {
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState({ first: false, second: false });
 
-  const handleLoad = () => {
-    setIsLoaded(true);
+  const handleLoad = (video) => {
+    setIsLoaded((prevState) => ({ ...prevState, [video]: true }));
   };
 
-  const YouTubeFacade = ({ videoId }: { videoId: string }) => (
+  const VideoComponent = ({ videoId, title, videoKey }) => (
     <div className="relative mb-10 lg:mb-0 mx-auto lg:mx-5 shadow-lightBlueShadow rounded-xl my-20 w-full min-h-[200px] sm:min-h-[300px] sm:min-w-[500px] 2xl:h-[400px] 2xl:w-[650px]">
-      {!isLoaded ? (
+      {!isLoaded[videoKey] ? (
         <div
           className="relative z-0 rounded-xl h-full bg-gray-200 min-h-[200px] sm:min-h-[300px] flex justify-center items-center cursor-pointer"
-          onClick={handleLoad}
+          onClick={() => handleLoad(videoKey)}
           style={{
             backgroundImage: `url(https://img.youtube.com/vi/${videoId}/hqdefault.jpg)`,
             backgroundSize: "cover",
@@ -27,13 +25,15 @@ function VideosSection() {
         </div>
       ) : (
         <iframe
-          loading="lazy"
-          className="mb-10 lg:mb-0 mx-auto lg:mx-5 shadow-lightBlueShadow rounded-xl my-20 w-full min-h-[200px] sm:min-h-[300px]  sm:min-w-[500px] 2xl:h-[400px] 2xl:w-[650px] "
+          width="1280"
+          height="720"
           src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player"
+          title={title}
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
+          className="mb-10 lg:mb-0 mx-auto lg:mx-5 shadow-lightBlueShadow rounded-xl my-20 w-full min-h-[200px] sm:min-h-[300px] sm:min-w-[500px] 2xl:h-[400px] 2xl:w-[650px]"
         ></iframe>
       )}
     </div>
@@ -41,28 +41,35 @@ function VideosSection() {
 
   return (
     <>
-      <div className="relative features flex  mt-40  lg:flex-row flex-col md:flex-wrap xl:flex-nowrap  justify-center  sm:gap-x-32 xl:gap-20">
+      <div className="relative features flex mt-40 lg:flex-row flex-col md:flex-wrap xl:flex-nowrap justify-center sm:gap-x-32 xl:gap-20">
         <div>
-          <YouTubeFacade videoId="a2LvxQtIdoo" />
-
-          <p className="sm:text-5xl 2xl:text-6xl  z-2 absolute text-3xl font-semibold text-left md:-ml-5 md:-mt-12 -mt-14 ">
-            How it works?
+          <VideoComponent
+            videoId="l1XjxVCDfLQ"
+            title="The best custom windows for gamers? ChlorideOS 10 SE"
+            videoKey="first"
+          />
+          <p className="sm:text-5xl 2xl:text-6xl z-2 absolute text-3xl font-semibold text-left md:-ml-5 md:-mt-12 -mt-14">
+            The best custom windows for gamers? ChlorideOS 10 SE
           </p>
         </div>
         <div className="max-h-full">
-          <YouTubeFacade videoId="a2LvxQtIdoo" />
+          <VideoComponent
+            videoId="wNrzCIhxEAs"
+            title="This is Windows 10 X - ChlorideOS X"
+            videoKey="second"
+          />
           <p className="sm:text-5xl 2xl:text-6xl z-20 absolute text-3xl font-semibold text-left md:-ml-5 md:-mt-12 -mt-14">
-            How to use?
+            This is Windows 10 X - ChlorideOS X
           </p>
         </div>
 
-        {/* backgound gradient */}
+        {/* background gradient */}
         <div
-          className="absolute sm:-left-32 top-0 left-0 sm:top-20 -z-10  blur-3xl "
+          className="absolute sm:-left-32 top-0 left-0 sm:top-20 -z-10 blur-3xl"
           aria-hidden="true"
         >
           <div
-            className="aspect-[1155/678] lg:w-[900px] w-[600px]  bg-gradient-to-br from-yellow-100 via-emerald-100 to-red-500 opacity-20 "
+            className="aspect-[1155/678] lg:w-[900px] w-[600px] bg-gradient-to-br from-yellow-100 via-emerald-100 to-red-500 opacity-20"
             style={{
               clipPath: "circle(50% at 5% 51%)",
             }}
