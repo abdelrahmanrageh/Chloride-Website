a256391218a71df292d6e0867f5e617fb0331f0d
ChlorideOS <86505707+ArabWep@users.noreply.github.com>
2024-12-26 01:42:41 -0800

    Update versionsData.ts

--- src/data/versionsData.ts
+++ src/data/versionsData.ts
@@ -5,30 +5,27 @@ export const versionsNames = [
   "OtherCustomReleases",
   "ChlorideArchive",
 ];
- 
 
 export const versions = {
-
   ChlorideOS10Versions: [
-       {
-      version: "ChlorideOS 10 SuperLight 21h2",
-      suitableFor: "Best windows 10 build ever released for gamers, Low, mid, high-End devices, Ready to use with all of the tools, apps you need",
-      whatsNew:"0 bloatwares ,Low CPU processes ( 45 ), Low RAM usage ( 0.8 ), 2.4GB iso. Thats 21h2 not 22h2, the file is named 22h2, Thats normal nvm",
+    {
+      version: "ChlorideOS 10 SuperLight 21H2",
+      suitableFor: "Best Windows 10 build for gamers. Ideal for low, mid, and high-end devices. Comes ready to use with all tools and apps you need.",
+      whatsNew: "0 bloatware, low CPU processes (45), low RAM usage (0.8GB), 2.4GB ISO. Note: File named '22H2' but it’s actually '21H2'.",
       price: "Free",
       link: "https://qiwi.gg/file/iSEi8629-ChlorideOS1022h2SuperLight",
-    },  
-{
-      version: "ChlorideOS 10 Extreme 22h2",
-      suitableFor: "The best for Mid, Low-End devices, Ready to use with all of the tools, apps you need",
-      whatsNew:"0 bloatwares ,Low CPU processes ( 70 - 90 ), Low RAM usage ( 0.9 - 1.2 GB ), 1.6GB iso",
+    },
+    {
+      version: "ChlorideOS 10 Extreme 22H2",
+      suitableFor: "Best for low and mid-end devices. Comes ready to use with all tools and apps you need.",
+      whatsNew: "0 bloatware, low CPU processes (70-90), low RAM usage (0.9-1.2GB), 1.6GB ISO.",
       price: "Free",
       link: "https://gofile.io/d/hRlmOq",
-    },  
+    },
     {
-      version: "ChlorideOS 10 Ultimate 22h2",
-      suitableFor: "Best ChlorideOS 10 Release You can find!",
-      whatsNew:
-        "0 bloatwares, 70 CPU processes , Low RAM usage ( 1.0 gb ), Only 5GB of disk usage, 1.5GB iso",
+      version: "ChlorideOS 10 Ultimate 22H2",
+      suitableFor: "Best ChlorideOS 10 release available.",
+      whatsNew: "0 bloatware, 70 CPU processes, low RAM usage (1.0GB), only 5GB disk usage, 1.5GB ISO.",
       price: "With Tweaks Ultimate Plan",
       link: "https://chlorideos.netlify.app/",
     },
@@ -36,19 +33,16 @@ export const versions = {
 
   ChlorideOS11Versions: [
     {
-      version: "ChlorideOS 24h2 Extreme v1.5.0",
-      suitableFor: "Low specs, old devices",
-      whatsNew:
-        "No bluetooth, extremly debloated, come with Store, Xbox, Powershell",
+      version: "ChlorideOS 24H2 Extreme v1.5.0",
+      suitableFor: "Low-spec and older devices.",
+      whatsNew: "No Bluetooth, extremely debloated, includes Store, Xbox, and PowerShell.",
       price: "Free",
       link: "https://ghmbybdabrezbrkai.xyz/d/xuci7mq5oghlriinozg3oqy3dnbqex6c3v26gsgqkzw2ux345qmqfkklesc74qshagavxatutho5mmi/ChlorideOS%2011%20Extreme%2024h2%201.5.0.iso",
     },
-
     {
-      version: "ChlorideOS 23h2 Ultimate SE",
-      suitableFor: "Lightest Version Ever, Best For Low End Devices",
-      whatsNew:
-        "100% Of bloatwares removed , lower cpu process ( 55-65 ) , lower ram usage ( 1.7 - 1.9 GB ), Only 12-14 GB of disk usage, 4GB iso",
+      version: "ChlorideOS 23H2 Ultimate SE",
+      suitableFor: "Lightest version ever. Best for low-end devices.",
+      whatsNew: "100% bloatware removed, lower CPU processes (55-65), lower RAM usage (1.7-1.9GB), only 12-14GB disk usage, 4GB ISO.",
       price: "With Tweaks Ultimate Plan",
       link: "https://chlorideos.netlify.app/",
     },
@@ -58,70 +52,62 @@ export const versions = {
     {
       version: "ChlorideTweaks Standard",
       suitableFor: "",
-      whatsNew: `Gain up to 20% FPS boost with usefull tools. Enjoy performance, network, power tweaks,
-       GPU optimization, registry enhancements, deep PC debloating.`,
+      whatsNew: "Gain up to 20% FPS boost with useful tools. Includes performance, network, power tweaks, GPU optimization, registry enhancements, and deep PC debloating.",
       price: "Free",
       link: "https://www.mediafire.com/file/cbxb31p5jiu0nur/Chloride_Tweaks_Standard_2.0.0.rar/file",
     },
     {
       version: "ChlorideTweaks Ultimate",
       suitableFor: "",
-      whatsNew: `
-         Get 100% free access to all ChlorideOS versions, Boost FPS by 30% with advanced scripts,
-        offering optimizations for performance, privacy, network, power, BIOS, GPU, USB, and more. Remove unnecessary apps,
-        optimize the registry, and enjoy deep PC debloating for a streamlined, efficient system. Elevate performance with
-        advanced power optimization techniques.`,
-      price: "5$",
+      whatsNew: "Access all ChlorideOS versions. Boost FPS by 30% with advanced scripts. Includes performance, privacy, network, power, BIOS, GPU, USB tweaks, and more. Optimize registry and enjoy advanced power optimizations.",
+      price: "$5",
       link: "https://chlorideos.netlify.app/",
     },
   ],
 
   OtherCustomReleasesVersions: [
-    
-     {
-      version: "AtlasOS11 23h2 iso",
+    {
+      version: "AtlasOS 11 23H2 ISO",
       suitableFor: "",
-      whatsNew: `AtlasOS 11 23H2 (ISO download) is here!, this new release has been rebuilt using the VMware for project,
-      AME Wizard Beta and AtlasOS playbook for creating AtlasOS! .`,
+      whatsNew: "AtlasOS 11 23H2 ISO built using VMware, AME Wizard Beta, and AtlasOS Playbook.",
       price: "Free",
       link: "https://archive.org/details/atlasos1123h2/Screenshot%20(2).png",
     },
     {
-      version: "AtlasOS10 21h2 iso",
+      version: "AtlasOS 10 21H2 ISO",
       suitableFor: "",
-      whatsNew: `AtlasOS 10 21H2 (ISO download) is here! Pass: 01234 .`,
+      whatsNew: "AtlasOS 10 21H2 ISO available! Password: 01234.",
       price: "Free",
       link: "https://drive.usercontent.google.com/download?id=1ecrgwe_YMMYtIXeBCdhw8hSeny-kSUVO&export=download&authuser=0",
-    },   
+    },
     {
-      version: "NanoOS 11 24h2",
+      version: "NanoOS 11 24H2",
       suitableFor: "",
-      whatsNew: `Better windows 11 24h2 release!.`,
+      whatsNew: "Improved Windows 11 24H2 release.",
       price: "Free",
       link: "https://nano11-dev.github.io/",
     },
- 
+  ],
 
-    ],
   ChlorideArchiveVersions: [
     {
-        version: "Chloride GPU Tweaking pack",
+      version: "Chloride GPU Tweaking Pack",
       suitableFor: "",
-      whatsNew: `Overclock your GPU and get the highest performance you can get, Pass: 123 .`,
+      whatsNew: "Overclock your GPU for maximum performance. Password: 123.",
       price: "Free",
       link: "https://www.mediafire.com/file/ubjdthbp73wq2z6/ChlorideOS+GPU+Tweaking+Pack.rar/file",
     },
     {
-      version: "Chloride processes lower",
+      version: "Chloride Processes Lower",
       suitableFor: "",
-      whatsNew: `Get more than 60 cpu process with onlu 1 click!, "Dont use with wifi", Pass: 123 .`,
+      whatsNew: "Reduce CPU processes to 60 with one click. Note: Do not use with Wi-Fi. Password: 123.",
       price: "Free",
       link: "https://www.mediafire.com/file/op75cau4qwk9pdj/ChlorideProcessesLower.rar/file",
     },
     {
       version: "Chloride Wallpapers",
       suitableFor: "",
-      whatsNew: `Get a package of nice ChlorideOS and Microsoft windows 11 style wallpapers! .`,
+      whatsNew: "Download a collection of ChlorideOS and Windows 11-style wallpapers.",
       price: "Free",
       link: "https://www.mediafire.com/folder/iuxuq1i0c4uhg/ChlorideWallpapers",
     },
